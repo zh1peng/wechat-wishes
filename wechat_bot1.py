e566bc846142892e0ec0412124d4e5507d0a2937
懒麻蛇 <25647407+zh1peng@users.noreply.github.com>
2018-03-22 12:40:23 +0000

    Update wechat_bot1.py

--- wechat_bot1.py
+++ wechat_bot1.py
@@ -6,7 +6,7 @@ import time
 
 KEY = '143da4e9713c42bca81b079cc0ff4d2f'
 
-def get_response(msg):
+def tuling_response(msg):
     # 这里我们就像在“3. 实现最简单的与图灵机器人的交互”中做的一样
     # 构造了要发送给服务器的数据
     apiUrl = 'http://www.tuling123.com/openapi/api'
@@ -33,21 +33,28 @@ def get_realtime(stop_id):
     result_list=result_json['results']
     bus_list=[i['route'] for i in result_list]
     due_time=[i['duetime'] for i in result_list]
-    msg=time.strftime('%H:%M:%S',time.gmtime())
+    bus_info=time.strftime('%H:%M:%S',time.gmtime())
     for routi,timei in zip(bus_list,due_time):
-        msg=msg+'\nRoute:%s---Duetime: %s' %(routi,timei)
-    return msg
-# 这里是我们在“1. 实现微信消息的获取”中已经用到过的同样的注册方法
-@itchat.msg_register(itchat.content.TEXT)
-def tuling_reply(msg):
-    defaultReply = 'I received: ' + msg['Text']
-    if msg.startswith('bus'):
-        stop_id=msg[3:]
+        bus_info=bus_info+'\nRoute:%s---Duetime: %s' %(routi,timei)
+    return bus_info
+
+def get_response(received_msg):
+    if received_msg.startswith('bus'):
+        stop_id=received_msg[3:]
         reply=get_realtime(stop_id)
     else:
-        reply = get_response(msg['Text'])
-    return reply or defaultReply
+        reply = tuling_response(received_msg)
+    return reply
+        
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply(msg):
+    received_msg=msg['Text']
+    try:
+        reply=get_response(received_msg)
+    except:
+        reply = '机器蛇好像挂了'
+    return reply
 
-# 为了让实验过程更加方便（修改程序不用多次扫码），我们使用热启动
+
 itchat.auto_login(enableCmdQR=2)
 itchat.run()
