592ef734eb52635cea18fd34fb38d759c507a29e
懒麻蛇 <25647407+zh1peng@users.noreply.github.com>
2018-03-23 10:09:19 +0000

    Update ebbinghaus.py

--- functions/ebbinghaus.py
+++ functions/ebbinghaus.py
@@ -1,1 +1,11 @@
 
+import datetime
+import time
+s = input('Enter learning date (YYYY/MM/DD) or Hit enter for "'"today"'": ')
+if s=='':
+    s=time.strftime("%Y/%m/%d")
+print('Dates to review are: ')
+for n in [1, 2, 4, 7, 15]:
+    d = datetime.datetime.strptime(s, '%Y/%m/%d') + datetime.timedelta(days=n)
+    print(d.strftime('%Y/%m/%d'))
+time.sleep(60)
