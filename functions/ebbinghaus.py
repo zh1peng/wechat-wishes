1650731cdcce14beb31c70d41d2f5ab920a528cc
懒麻蛇 <25647407+zh1peng@users.noreply.github.com>
2018-03-24 10:39:23 +0000

    Update ebbinghaus.py

--- functions/ebbinghaus.py
+++ functions/ebbinghaus.py
@@ -1,4 +1,5 @@
 
+# print review time
 import datetime
 import time
 s = input('Enter learning date (YYYY/MM/DD) or Hit enter for "'"today"'": ')
@@ -9,3 +10,29 @@ for n in [1, 2, 4, 7, 15]:
     d = datetime.datetime.strptime(s, '%Y/%m/%d') + datetime.timedelta(days=n)
     print(d.strftime('%Y/%m/%d'))
 time.sleep(60)
+
+
+# thread1
+import datetime
+import time
+def attach_review_time(msg):
+    now = datetime.datetime.now()
+    #20min, 1hour, 8~9hour 1days 2days 6days 15days
+    review_times=[]
+    review_dic={}
+    for review_time in [20, 60, 480,1440,2880,8640,21600]:
+        tmp = now + datetime.timedelta(minutes = review_time)
+        review_times.append(tmp.strftime("%Y%m%d%H%M%S"))
+        review_dic[tmp.strftime("%Y%m%d%H%M%S")]=msg
+    return review_dic
+time.sleep()
+review_dic=attach_review_time(msg)  
+all2review={}
+all2review.update(a)
+
+# Thread 2
+def thread2(send2review)
+while True: 
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    if now in all2review:
+        print(all2review[now])
