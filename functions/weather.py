e7b5573a01bf29a9fe812c90b7c35ea2eb85300e
懒麻蛇 <25647407+zh1peng@users.noreply.github.com>
2018-03-28 14:47:23 +0100

    Create weather.py

---
+++ functions/weather.py
@@ -0,0 +1,23 @@
+from weather import Weather, Unit
+weather = Weather(unit=Unit.CELSIUS)
+
+# Lookup WOEID via http://weather.yahoo.com.
+
+lookup = weather.lookup(560743)
+condition = lookup.condition()
+print(condition.text())
+
+# Lookup via location name.
+
+location = weather.lookup_by_location('dublin')
+condition = location.condition()
+print(condition.text())
+
+# Get weather forecasts for the upcoming days.
+
+forecasts = location.forecast()
+for forecast in forecasts:
+    print(forecast.text())
+    print(forecast.date())
+    print(forecast.high())
+    print(forecast.low())
